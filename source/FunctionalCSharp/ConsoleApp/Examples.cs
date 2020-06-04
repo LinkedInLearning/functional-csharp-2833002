f6acbbe65653c72e49badabe4d651b08503ea6c6
walt-instructor <staffauthor@outlook.com>
2020-06-04 16:07:36 -0700

    Add Sample Code

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace ConsoleApp
 {
-	class Examples
+	public class Examples
 	{
-
+		// Pure functions are similar to mathematical functions
+		// they compute an output value based on their input values. 
+		// the do nothing else in the code
+		public long CalcExponent(long originalNumber, long exponent)
+		{
+			return originalNumber ^ exponent;
+		}
 
 	}
 }
--- source/FunctionalCSharp/UnitTests/Tests.cs
+++ source/FunctionalCSharp/UnitTests/Tests.cs
@@ -1,13 +1,25 @@
+using ConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
 	[TestClass]
-	public class Tests
+	public class APureFunction_Should
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void ReturnSameResults_WhenSameInputs1()
 		{
+			// arrange
+			var examples = new Examples();
+			long value = 5;
+			long exponent = 3;
+			// act
+			var result1 = examples.CalcExponent(value, exponent);
+			var result2 = examples.CalcExponent(value, exponent);
+
+			// assert
+			Assert.AreEqual((value ^ exponent), result1);
+			Assert.AreEqual(result1, result2);
 		}
 	}
 }
