73f3e5708bbf18ad260bda985bae0a5ea20b6eb6
walt-instructor <staffauthor@outlook.com>
2020-06-05 14:41:24 -0700

    Add comments

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -6,8 +6,7 @@ namespace ConsoleApp
 {
 	public class Examples
 	{
-		private int _counter = 0;
-
+	
 
 		// avoid side effects in functions
 		// A side effect is a change of system state 
@@ -15,31 +14,23 @@ namespace ConsoleApp
 		// that occurs during the calculation of a result.
 
 		// a function should not 
-		// 1. Mutate shared state
 		// 2. Mutate its input arguments
-		// 3. Throw exceptions
-		// 4. Perform any I/O operation
-		public void UpdateByTwo()
-		{
-			_counter += 2;
-		}
-		public void UpdateByFive()
-		{
-			_counter += 5;
-		}
+		// 
+		//
+	
 
 		#region List as shared state
 
 
 		private List<int> _numbers = new List<int> { 1, 3, 5, 7 };
 
-		public void AddNumbersToList()
+		public void AddNumbersToListOld()
 		{
 			_numbers.Add(2);
 			_numbers.Add(4);
 		}
 
-		public int TotalTheNumbers()
+		public int TotalTheNumbersOld()
 		{
 			int total = 0;
 			foreach (int number in _numbers)
@@ -50,6 +41,35 @@ namespace ConsoleApp
 		}
 		#endregion
 
+		#region Don't mutate input arguments
+		public void DoWork()
+		{ 
+
+			
+			var numbersList = new List<int> { 1, 3, 5, 7 };
+			AddNumbersToList(numbersList);
+			var total = TotalTheNumbers(numbersList);
+			Console.WriteLine(total);
+		}
+
+		public void AddNumbersToList(List<int>inputList)
+		{
+			// Pure functions should treat inputs 
+			// as immutable
+			// Immutable types are a core concept for FP
+			inputList.Add(2);
+			inputList.Add(4);
+		}
+		public int TotalTheNumbers(List<int> inputList)
+		{
+			int total = 0;
+			foreach (int number in inputList)
+			{
+				total += number;
+			}
+			return total;
+		}
+		#endregion
 
 	}
 }
