29ee54456776c94307229dc29d1c29ae701b2960
walt-instructor <staffauthor@outlook.com>
2020-06-05 11:54:34 -0700

    Remove empty unit tests

--- source/FunctionalCSharp/UnitTests/Tests.cs
+++ source/FunctionalCSharp/UnitTests/Tests.cs
@@ -2,10 +2,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ConsoleApp;
 namespace UnitTests
 {
-	[TestClass]
+	
 	public class Tests
 	{
-		[TestMethod]
+	
 		public void TestMethod1()
 		{
 		}
