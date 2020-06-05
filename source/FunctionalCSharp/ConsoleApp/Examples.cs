56269ae881dbaa4df168b927880cfbd53c0c758b
walt-instructor <staffauthor@outlook.com>
2020-06-04 21:16:48 -0700

    Add example code

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp
 {
 	class Examples
 	{
+		// this is an impure function because it returns different
+		// values given the same inputs.
 
+		// also this code is not easily testable
+		public DateTime GetCurrentTimeRoundedUpToCustomMinuteInterval(int interval)
+		{
+			var currentTime = DateTime.Now;
+			var minutesSpan = TimeSpan.FromMinutes(interval).Ticks;
+
+			if (currentTime.Ticks % minutesSpan == 0)
+			{
+				return currentTime;
+			}
+			else
+			{
+				return new DateTime((currentTime.Ticks / minutesSpan + 1) * minutesSpan);
+			}
+
+		}
 
 	}
 }
--- source/FunctionalCSharp/ConsoleApp/Program.cs
+++ source/FunctionalCSharp/ConsoleApp/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp
 {
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
-			// all demo code is in the repository branches
-			Console.WriteLine("Hello World!");
+
+			Timer t = new Timer(TimerCallback, null, 0, 1000);
+			Console.ReadLine();
+			
 		}
+			private static void TimerCallback(Object o)
+			{
+			var examples = new Examples();
+			Console.Clear();
+			Console.WriteLine(DateTime.Now);
+			var result = examples.GetCurrentTimeRoundedUpToCustomMinuteInterval(12);
+			Console.WriteLine(result);
+		}
+		
 	}
 }
