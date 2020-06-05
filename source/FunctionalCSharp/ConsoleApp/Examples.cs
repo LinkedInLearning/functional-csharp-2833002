99b43665db74184814de7462f2d2e34de45f11e9
walt-instructor <staffauthor@outlook.com>
2020-06-05 11:26:02 -0700

    Add example code for mutating shared state

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -4,9 +4,51 @@ using System.Text;
 
 namespace ConsoleApp
 {
-	class Examples
+	public class Examples
 	{
+		private int _counter = 0;
 
 
-	}
+		// avoid side effects in functions
+		// A side effect is a change of system state 
+		// or observable interaction with the outside world
+		// that occurs during the calculation of a result.
+		
+		// a function should not 
+		// 1. Mutate shared state
+		// 2. Mutate its input arguments
+		// 3. Throw exceptions
+		// 4. Perform any I/O operation
+		public void UpdateByTwo()
+		{
+			_counter += 2;
+		}
+		public void UpdateByFive()
+		{
+			_counter += 5;
+		}
+
+		#region List as shared state
+
+
+		private List<int> _numbers = new List<int> { 1, 3, 5, 7 };
+
+		public void AddNumbersToList()
+		{
+			_numbers.Add(2);
+			_numbers.Add(4);
+		}
+
+		public int TotalTheNumbers()
+		{
+			int total =0;
+			foreach (int number in _numbers)
+			{
+				total += number;
+			}
+			return total;
+		}
+		#endregion
+
+	
 }
