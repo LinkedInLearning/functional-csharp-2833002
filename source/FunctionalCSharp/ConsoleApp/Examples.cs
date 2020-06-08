ccd2722dfc763793d1bc115611c9b7cbe1f0a148
walt-instructor <staffauthor@outlook.com>
2020-06-08 15:10:08 -0700

    Add expression example

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -6,7 +6,40 @@ namespace ConsoleApp
 {
 	class Examples
 	{
+		public void ExamplesOfExpressions() {
+			//An expression yields a value 
+			// and can be used in places where a value is expected
 
+			// variables to hold the "value"
+
+			string stringValue;
+			int intValue;
+
+			// literals are expressions
+
+			stringValue = "hello";
+			intValue = 909;
+
+			// variables are expression
+			string hello = stringValue;
+			int x = intValue;
+
+			// invocations (function calls that return a values)
+
+			var upperCaseWord = stringValue.ToUpper();
+			var concatValue = $"first number{intValue}, second number{x}";
+
+			double calculatedValue = Math.Sqrt(Math.Abs(3) + x);
+
+			// operators and operands
+
+			bool isBig = x > 1000;
+			string isBigString = isBig ? "Big Number" : "Small number"
+;
+
+		}
+
+		// Expressions include anything that produces a value, such as these:
 
 	}
 }
