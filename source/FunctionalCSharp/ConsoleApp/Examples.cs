dba8ece3611bd3e7686c7874d6b5222c8c72f158
walt-instructor <staffauthor@outlook.com>
2020-06-09 20:59:18 -0700

    Add higher order example

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
@@ -20,42 +21,51 @@ namespace ConsoleApp
 		// are the most useful for functional programming
 		public void DoWork()
 		{
-			UseLambda();
+			UseHigherOrderFunction();
 
 		}
 
-		private  void UseLambda()
+		public void UseHigherOrderFunction()
 		{
-			// easy way to represent functions is with 
-			// the Func<T> and Action<T> delegates
+			// a  function that take a function argument 
+			// or returns a function
+			// is a higher order function
 
-			// Use variable to store a function (as delegate type)
-			// If your function is short, and doesn't need to be reused
-			// a Lambda expression is useful
-			Func<int, int> functionVar = x => (x * 10);
+			var numbers = Enumerable.Range(1, 120);
 
 
-			int result = functionVar(6);
-
-			Func<int, int, bool> predicateVar = IsMultipleOf;
-
-
-			bool isMultipleOfFive = predicateVar(25, 5);
-
-			bool isMultipleOfSeven = predicateVar(25, 7);
+			var divisibleByFive = numbers.FilterWithWhereExpression(x => x % 5 == 0);
+			var divisibleBySeven = numbers.FilterWithWhereExpression(x => x % 7 == 0);
 
 		}
 
-		// or create method when there is more code
-		// or we need to call from other locations.
 
-		
 
 		public bool IsMultipleOf(int candidate, int multiplier)
 
 		{
 			return (candidate % multiplier) == 0;
 		}
-		
+
+	}
+	public static class Extensions
+	{
+		// use delegates as function arguments
+
+		public static IEnumerable<T> FilterWithWhereExpression<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+		{
+
+			foreach (T item in items)
+			{
+				if (predicate(item))
+				{
+					yield return item;
+				}
+				{
+
+				}
+			}
+
+		}
 	}
 }
