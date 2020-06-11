36b1ad9be739bbcae5ded7ed0491512d9d02aed0
walt-instructor <staffauthor@outlook.com>
2020-06-10 20:10:33 -0700

    Remove generic code

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -9,31 +9,11 @@ namespace ConsoleApp
 	class Examples
 	{
 
-		public void DoWorkWithStandardLambda()
-		{
-			Func<int, int> toPowerFour = x => x * x * x * x;
-			Func<int, int> makeNegative = x => -1 * x;
-	
-
-			int value = 5;
-			int resultA = value.PerformOperation(toPowerFour).PerformOperation(makeNegative).PerformOperation(x=> x + 3);
-			double doubleVal = 7.5;
-			double resultB = doubleVal.PerformOperation(x => Math.Sin(x)).PerformOperation(x => x * Math.PI);
-		}
 
 
 		public void DoWorkWithPipeine()
 		{
-		
 
-
-			//int resultA = addTo(toPowerFour(makeNegative(5)), 3);
-
-			//// compose a new function
-
-			//var composed = makeNegative.Compose(toPowerFour));
-			//// var composed2 = makeNegative.Compose(toPowerFour).Compose(addTo);
-			//int resultB = composed(5);
 
 			int value = 5;
 			int resultA = value.ToPowerFour().MakeNegative();
@@ -42,7 +22,6 @@ namespace ConsoleApp
 			
 		}
 
-		
 
 	}
 
@@ -63,10 +42,6 @@ namespace ConsoleApp
 		{
 			return candidate + adder;
 		}
-		public static T PerformOperation<T>(this T value, Func<T, T> performer) where T : struct
-		{
 		
-			return performer(value);
-		}
 	}
 }
--- source/FunctionalCSharp/ConsoleApp/Program.cs
+++ source/FunctionalCSharp/ConsoleApp/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp
 		{
 
 			var examples = new Examples();
-			examples.DoWorkWithStandardMethods();
+
 			examples.DoWorkWithPipeine();
 		}
 	}
