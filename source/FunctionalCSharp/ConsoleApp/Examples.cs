40492910b4638438e5f5a882aba5156b2fbd9caf
walt-instructor <staffauthor@outlook.com>
2020-06-11 22:13:45 -0700

    Sample code added

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -1,53 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ConsoleApp
 {
-	class Examples
+	internal class Examples
 	{
-
-
-		public void FilterSimple()
+		public void FlattenListProperty()
 		{
-			// functional Filter == LINQ Where
-			// return a subset of the list based 
-			// on predicate function
+			// functional bind (in Haskell)
+			// called flatMap (in Scala)
+			// and is concept is called flattening in C#
+			// LINQ SelectMany or ContinueWith
 
-			var numbers = Enumerable.Range(1, 200);
+			// flattens a multi-dimensional set into a single set
 
-			var queryA = numbers.Where(x => x < 20).Select(x => x);
+			// or another way to say
+			// SelectMany select values from a nested collection
 
-			var queryB = from n in numbers
-									 where n < 20 || n > 180
-									 select n;
+			var brandA = new Brand { Name = "Fancy-Shoes", Colors = new List<string> { "Red", "Orange" } };
+			var brandB = new Brand { Name = "Lux-Cars", Colors = new List<string> { "Gold", "Silver" } };
+			var brandC = new Brand { Name = "Wow-Electronics", Colors = new List<string> { "Black", "Blue", "Purple" } };
+			List<Brand> brands = new List<Brand>();
+			brands.Add(brandA);
+			brands.Add(brandB);
+			brands.Add(brandC);
 
-			var resultsA = queryA.ToList();
-			var resultsB = queryB.ToList();
-
+			var resultA = brands.Select(x => x).ToList();
+			var resultB = brands.SelectMany(x => x.Colors).ToList();
 		}
 
-		public void FilterForPrimeNumbers()
+		public void SelectManyExample()
 		{
-			// this predicate determines if 
-			// a number is prime
+			var setA = Enumerable.Range(2, 5);
+			var setB = Enumerable.Range(-7, 5);
 
-			Func<int, bool> isPrime = p => Enumerable.Range(2, (int)Math.Sqrt(p) - 1)
-																		.All(divisor => p % divisor != 0);
+			var basicSelect = setA.Select(n => $"value {n}");
+			var basicJoin = setA.SelectMany(x => setB.Select(y => $"X:{x} Y:{y}"));
 
-			var primes =
-				Enumerable.Range(2, 1000 * 1000)
-				.Where(isPrime);
+			var cartesianLst = setA.SelectMany(a => setB.Select(b => a + " X " + b + "=" + a * b));
 
-			var resultsA = primes.ToList();
-			
-
+			var resultsA = basicSelect.ToList();
+			var resultsB = basicJoin.ToList();
 		}
-
-
 	}
 
-	
+	public static class Extensions
+	{
+		// signature looks like this
+		public static IEnumerable<B> SelectMany_Example<A, B>(this IEnumerable<A> first, Func<A, IEnumerable<B>> selector)
+		{
+			return null;
+		}
+	}
+
+	// this in not immutable yet
+
+	public class Brand
+	{
+		public string Name { get; set; }
+		public List<string> Colors { get; set; }
 	}
 }
--- source/FunctionalCSharp/ConsoleApp/Program.cs
+++ source/FunctionalCSharp/ConsoleApp/Program.cs
@@ -8,9 +8,9 @@ namespace ConsoleApp
 		{
 			// all demo code is in the repository branches
 			var examples = new Examples();
-			examples.FilterSimple();
+			examples.SelectManyExample();
+			examples.FlattenListProperty();
 		
-			examples.FilterForPrimeNumbers();
 		}
 	}
 }
